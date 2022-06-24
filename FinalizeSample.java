6e782058a0e76004f58c7a0a6b84a6d4263d8432
alex-jeffrin <ajjeffrinalex@gmail.com>
2022-06-24 11:25:44 +0530

    finalize method overloading

--- FinalizeSample.java
+++ FinalizeSample.java
@@ -8,25 +8,23 @@ class createUser {
     }
 
     protected void finalize(){
+
         System.out.println("The user account is unlinked and deleted");
+
     }
+
 }
 
-class FinalizeSamle{
+class FinalizeSample {
     public static void main (String args[]){
         createUser user1 = new createUser();
         createUser user2 = new createUser(); //There are two users created
-        createUser user1 = new createUser();
+        
+        new createUser();
 
         user1 = null;
 
         System.gc(); // the user1 is deleted cause its value is null so it no more exists
-
-        createUser user1 = new createUser();
-
-
-
-
 
     }
 }
